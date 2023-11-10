d04d538693d7a446612718a2be6b862afe8d8ddf
Paul McKillop <pgmckillop@gmail.com>
2023-11-10 18:14:58 +0000

    Project model registered to allow access.

--- site_admin/projects/admin.py
+++ site_admin/projects/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+"""
+Register the Project model for use in the admin panel.
+The admin panel will allow direct access to the sqlite database.
+"""
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    search_fields = ['title', 'description_short', 'description']
+    list_filter = ['title']
